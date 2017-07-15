bac2e4fdfc0ea207bfa062feb75e42762ebc7d75
peter8472 <i60IV27@outlook.com>
2017-07-15 14:14:41 -0700

    Well it works now.  Just need to add menu bar

--- captionReceiver/MainWindow.xaml.cs
+++ captionReceiver/MainWindow.xaml.cs
@@ -12,17 +12,94 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+
 
 namespace captionReceiver
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        private Socket blah;
+        private int portnum;
+        private delegate void myDelegate();
+        private delegate void oneArgDelegate(String answer);
+        
+
+
         public MainWindow()
         {
+            Random portGen = new Random();
+            //portnum = portGen.Next(49152, 65535);
+            portnum = 1200;
             InitializeComponent();
+            IPHostEntry host;
+            String hostName = Dns.GetHostName();
+            host = Dns.GetHostEntry(hostName);
+            TextBox box = textBox;
+            box.Text = "";
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    box.Text += String.Format("server {0} port {1}\n", ip.ToString(), portnum);
+                    
+                }
+            }
+            
+            
+            myDelegate blah = new myDelegate(message);
+            blah.BeginInvoke(null, null);
+            
+
+
+
+
+        }
+        public void message()
+        {
+            const int BUFMAX = 10000;
+            IPEndPoint end = new IPEndPoint(IPAddress.Any, portnum);
+            TcpListener l = new TcpListener(end);
+            byte[] buffer = new byte[BUFMAX];
+            int numBytes = 0;
+            l.Start();
+            while (true)
+            {
+                blah = l.AcceptSocket();
+                int bufferindex = 0;
+                while (blah.Connected)
+                {
+                    numBytes = blah.Receive(buffer, bufferindex, BUFMAX - bufferindex, 0);
+                    if (numBytes == 0)
+                        break;
+                    bufferindex = bufferindex + numBytes;
+
+                }
+
+                String mess = Encoding.ASCII.GetString(buffer, 0, bufferindex);
+
+                this.Dispatcher.BeginInvoke(
+                    System.Windows.Threading.DispatcherPriority.Normal,
+                    new oneArgDelegate(updateUserInterface), mess);
+
+
+            }
+        }
+        private void updateUserInterface(String data)
+        {
+            TextBox box = textBox;
+            if (!data.EndsWith("\n"))
+            {
+                data += "\n";   
+            }
+            box.Text += data;
         }
     }
 }
